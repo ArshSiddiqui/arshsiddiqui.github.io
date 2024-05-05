a46946e8d4bd6d42bbe3cfea75373b7c6330ed28
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2024-05-05 19:00:20 -0400

    Add files via upload

--- scrobbling.py
+++ scrobbling.py
@@ -4,6 +4,49 @@ import json
 userName = 'arshsiddiqui'
 apiKey = '90874a2969204e8c7da1d789f5e90638'
 
+res = requests.get(f'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user={userName}&api_key={apiKey}&format=json')
+res.encoding = 'utf-8'
+response = json.loads(res.text.encode('utf-8'))
+
+top_artists = []
+top_artists_url = []
+for i in range(0, 3):
+    top_artists.append(response['topartists']['artist'][i]['name'])
+    top_artists_url.append(response['topartists']['artist'][i]['url'])
+
+json_out = '['
+for i in range(0, 3):
+    json_out += f'{{"name": "{top_artists[i]}", "link": "{top_artists_url[i]}"}}'
+    if i < 2:
+        json_out += ','
+json_out += ']'
+
+f = open("top_artists.json", 'w', encoding='utf8')
+f.write(str(json_out))
+f.close()
+
+res = requests.get(f'http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user={userName}&api_key={apiKey}&format=json') 
+res.encoding = 'utf-8'
+response = json.loads(res.text.encode('utf-8'))
+
+top_albums = []
+top_albums_url = []
+for i in range(0, 3):
+    top_albums.append(response['weeklyalbumchart']['album'][i]['name'])
+    top_albums_url.append(response['weeklyalbumchart']['album'][i]['url'])
+
+json_out = '['
+for i in range(0, 3):
+    json_out += f'{{"name": "{top_albums[i]}", "link": "{top_albums_url[i]}"}}'
+    if i < 2:
+        json_out += ','
+json_out += ']'
+
+f = open("top_albums.json", 'w', encoding='utf8')
+f.write(str(json_out))
+f.close()
+
+
 res = requests.get(f'https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user={userName}&api_key={apiKey}&period=7day&limit=10&format=json')
 res.encoding = 'utf-8'
 response = json.loads(res.text)
@@ -28,7 +71,7 @@ for i in range(0, len(chansons)):
         json_out += ','
 json_out += ']'
 
-f = open("past_week_scrobble.json", 'w', encoding='utf8')
+f = open("last_week_scrobble.json", 'w', encoding='utf8')
 f.write(str(json_out))
 f.close()
 
